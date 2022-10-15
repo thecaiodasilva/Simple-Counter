2ab563eebb3ba2babcd1aaf3bd5c5cf4c970f275
Caio da Silva <66576487+thecaiodasilva@users.noreply.github.com>
2022-10-15 14:44:46 -0300

    up new css e javascript

--- assets/scripts.js
+++ assets/scripts.js
@@ -10,3 +10,8 @@ function decrement(){
 	currentNumber = currentNumber - 1;
 	currentNumberWrapper.innerHTML = currentNumber;
 }
+
+function reset(){
+	currentNumber = 0 ;
+	currentNumberWrapper.innerHTML = currentNumber;
+}
